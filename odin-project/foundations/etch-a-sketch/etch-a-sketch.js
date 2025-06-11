27061785dec1c68657be5ee7870aca0be3942e70
Victor Yuan <victor.2wy@gmail.com>
2025-06-11 11:16:24 -0700

    16x16 grid

--- odin-project/foundations/etch-a-sketch/etch-a-sketch.js
+++ odin-project/foundations/etch-a-sketch/etch-a-sketch.js
@@ -5,17 +5,22 @@ const grid = document.querySelector('#grid');
 
 
 function createGrid(grid, x = 16, y = 16){
-  // one row
-  let oneRow = createRow(ncol = y);
-  grid.appendChild(oneRow);
+  for (let i = 0; i < x-1; i++) {
+    let row = createRow(ncol = y);
+    grid.appendChild(row);
+  }
 }
 
 
 function createRow(ncol = 16) {
+
+  // create a div for each row
+  // each row will have class row, with display: flex
   let row = document.createElement("div");  
   row.classList.add('row');
 
   for (let i = 0; i < ncol-1; i++) {
+    // each square has flex: 1
     const square = document.createElement("div");  
     square.classList.add("square");
     row.appendChild(square)
