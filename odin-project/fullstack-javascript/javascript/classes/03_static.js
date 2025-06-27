2705d357a638b0f9d59e3557fd67dd1aba197f64
Victor Yuan <victor.2wy@gmail.com>
2025-06-27 10:25:13 -0700

    classes

---
+++ odin-project/fullstack-javascript/javascript/classes/03_static.js
@@ -0,0 +1,28 @@
+class Person {
+
+  static species() {
+    return 'Homo sapiens';
+  }
+
+  static speciesSentence() {
+    return `Humans are ${this.species()}`
+  }
+
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.hasJob = false;
+  }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  setFirstName(firstName) {
+    this.firstName = firstName;
+  }
+
+  setLastName(lastName) {
+    this.lastName = lastName;
+  }
+}
